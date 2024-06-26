ea4bc45dd0fca93b07d57d0b6487ecc71ec51b58
Arqam Zia <i221170@nu.edu.pk>
2024-06-27 01:04:09 +0500

    Updated

--- sync-directories.sh
+++ sync-directories.sh
@@ -97,3 +97,17 @@ rsync -av --delete "$SOURCE_DIR" "$DEST_DIR"
 
 # Print a message indicating the process is complete
 echo "Synchronization and copy complete from $SOURCE_DIR to $DEST_DIR"
+
+###############################################
+#Cava
+###############################################
+
+# Define the source and destination directories
+SOURCE_DIR=~/.config/cava/
+DEST_DIR=~/dotfiles-nixos-hyprland/cava/
+
+# Use rsync to copy and synchronize the contents
+rsync -av --delete "$SOURCE_DIR" "$DEST_DIR"
+
+# Print a message indicating the process is complete
+echo "Synchronization and copy complete from $SOURCE_DIR to $DEST_DIR"
