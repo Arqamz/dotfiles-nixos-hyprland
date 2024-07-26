a4b0bb54ceecf50499729f1f01afe44861279071
Arqam Zia <arqam.mzia@gmail.com>
2024-07-26 23:44:57 +0500

    Committing change for ags/config.js

--- ags/config.js
+++ ags/config.js
@@ -1,61 +1,92 @@
-import App from 'resource:///com/github/Aylur/ags/app.js';
-import { exec } from 'resource:///com/github/Aylur/ags/utils.js'
-import { forMonitors } from './Common.js';
-import { monitorFile } from 'resource:///com/github/Aylur/ags/utils.js';
-import { ActivityCenter } from './Windows/ActivityCenter.js';
-import { NotificationPopup } from './Windows/NotificationPopups.js';
-import { Dock } from './Windows/Dock.js';
-import { Lockscreen } from './Windows/Lockscreen.js';
-import { Launcher } from './Windows/Launcher.js';
-import { Bar } from './Windows/Bar.js';
-import { ControlPanel } from './Windows/ControlPanel.js';
-import { Settings } from './Windows/Settings.js';
+import GLib from "gi://GLib"
 
-// GDK Display
-import Gdk from 'gi://Gdk'
-const display = new Gdk.Display()
+const main = "/tmp/ags/hyprpanel/main.js"
+const entry = `${App.configDir}/main.ts`
+const bundler = GLib.getenv("AGS_BUNDLER") || "bun"
 
-// Add icons in assets to icon set
-//Gtk.IconTheme.get_default().append_search_path(`${App.configDir}/assets`);
-App.addIcons(`${App.configDir}/assets`)
+const v = {
+    ags: pkg.version?.split(".").map(Number) || [],
+    expect: [1, 8, 1],
+}
 
-// main scss file
-const scss = `${App.configDir}/Style/style.scss`
+try {
+    switch (bundler) {
+        case "bun": await Utils.execAsync([
+            "bun", "build", entry,
+            "--outfile", main,
+            "--external", "resource://*",
+            "--external", "gi://*",
+            "--external", "file://*",
+        ]); break
 
-// target css file
-const css = `${App.configDir}/Style/style.css`
+        case "esbuild": await Utils.execAsync([
+            "esbuild", "--bundle", entry,
+            "--format=esm",
+            `--outfile=${main}`,
+            "--external:resource://*",
+            "--external:gi://*",
+            "--external:file://*",
+        ]); break
 
-// Generate css
-exec(`sassc ${scss} ${css}`)
+        default:
+            throw `"${bundler}" is not a valid bundler`
+    }
 
-monitorFile(
-    `${App.configDir}/Style/_colors.scss`,
-    function() {
-        exec(`sassc ${scss} ${css}`)
-        App.resetCss();
-        App.applyCss(`${App.configDir}/Style/style.css`);
-    },
-);
+    if (v.ags[1] < v.expect[1] || v.ags[2] < v.expect[2]) {
+        print(`my config needs at least v${v.expect.join(".")}, yours is v${v.ags.join(".")}`)
+        App.quit()
+    }
 
-App.config({
-    style: css, 
-    closeWindowDelay: {
-        "ControlPanel":     150, // milliseconds
-        "applauncher":      150, // milliseconds
-        "ActivityCenter":   150, // milliseconds
-    },
-    // What does ... do? Spread syntax allows you to deconstruct an array or object into separate variables.
-    // ... here returns the array output of forMonitors as a individual elements so they are not nested in the parrent array
-    windows: [
-        Launcher(), 
-        //...forMonitors(Bar), 
-        Bar(),
-        Lockscreen(), 
-        ControlPanel(),
-        ActivityCenter(),
-        NotificationPopup(), 
-        Settings(),
-        //Dock()
-    ],
-});
+    await import(`file://${main}`)
+} catch (error) {
+    console.error(error)
+    App.quit()
+}
 
+export { }
+// import { exec } from "resource:///com/github/Aylur/ags/utils.js";
+// import { Bar, BarAlt } from "./modules/bar/index.js";
+// import DirectoryMonitorService from "./directoryMonitorService.js";
+// import MenuWindows from "./modules/menus/main.js";
+// import Notifications from "./modules/notifications/index.js";
+//
+// const applyScss = () => {
+//   // Compile scss
+//   exec(`sass ${App.configDir}/scss/main.scss ${App.configDir}/style.css`);
+//   exec(
+//     `sass ${App.configDir}/scss/highlight.scss ${App.configDir}/highlight.css`,
+//   );
+//   console.log("Scss compiled");
+//
+//   // Apply compiled css
+//   App.resetCss();
+//   App.applyCss(`${App.configDir}/style.css`);
+//   console.log("Compiled css applied");
+// };
+//
+// DirectoryMonitorService.connect("changed", () => applyScss());
+//
+// applyScss();
+//
+// const workspaceMonitorMap = {
+//   0: [4, 5],
+//   1: [6, 7],
+//   2: [1, 2, 3, 8, 9, 10],
+// };
+//
+// App.config({
+//   windows: [
+//     ...MenuWindows,
+//     Notifications(),
+//     BarAlt(0, workspaceMonitorMap),
+//     BarAlt(1, workspaceMonitorMap),
+//     Bar(2, workspaceMonitorMap),
+//   ],
+//   style: `${App.configDir}/style.css`,
+//   closeWindowDelay: {
+//     sideright: 350,
+//     launcher: 350,
+//     bar0: 350,
+//   },
+//   onConfigParsed: () => Utils.execAsync(`python3 ${App.configDir}/services/bluetooth.py`),
+// });
